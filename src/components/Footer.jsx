aabf9686f07f56384cf40cfc91ca5e2230aeda09
jscriptural <isongjohn014@gmail.com>
2024-02-13 13:15:41 +0100

    added social links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,11 +4,11 @@ import {navLinks} from "./Header.jsx";
 const socials = [
   {
     platform: "Facebook",
-    link: "#",
+    link: "https://www.facebook.com/trenzlinks?mibextid=ZbWKwL",
   },
   {
     platform: "Twitter",
-    link: "#",
+    link: "https://x.com/trenzlinks?t=RNmDx4Xya_7XLm1pvMxlvQ&s=09",
   }
 ];
 
@@ -17,14 +17,14 @@ export default function Footer(){
   return (
     <section id="footer">
       <div className="socials">
-	<h1>Follow Us</h1>
+	<h2>Follow Us</h2>
 	<ul>
 	  {socials.map((social,i)=>(<li key={i} className={social.platform+" social"}> <a href={social.link} target="_blank" title={"follow us on "+social.platform}>{social.platform}</a></li>
 	  ))}
 	</ul>
       </div>
       <div className="categories">
-	<h1> Categories </h1>
+	<h2> Categories </h2>
 	<ul>
 	  {navLinks.map((link,i)=>(<li className="nav-links" key={i}>
 	    <Link className="link" to={link.toLowerCase()}>{link}</Link>
@@ -32,6 +32,7 @@ export default function Footer(){
 	  ))}
 	</ul>
       </div>
+      <div className="contact"><h3>Send us a mail:</h3> <p> trenznewssite@gmail.com </p></div>
       <div className="copyright"><span>&copy;{new Date().getFullYear()}</span></div>
     </section>
   );
