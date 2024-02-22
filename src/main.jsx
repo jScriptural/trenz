14ef9dea25a65a0bf6171412053c1b43370cfde8
jscriptural <isongjohn014@gmail.com>
2024-02-22 20:17:30 +0100

    rm analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {inject} from "@vercel/analytics"
 import App from './App.jsx'
 import './assets/scss/index.scss'
 import {BrowserRouter as Router} from "react-router-dom";
 
 
-inject();
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    <Router>
